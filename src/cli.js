02beb1c3d27a1bc3c537d735cdfbfc5cc5eaf904
Alex Wishinkiy <basstraper@gmail.com>
2023-08-25 16:37:27 +0300

    Update cli.js
    edit welcome

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,12 @@
 import readlineSync from 'readline-sync';
 
-const userName = readlineSync.question('May I have your name? ');
+
 
 function greetingsUser() {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
+  
 }
 
 export { greetingsUser, userName };
