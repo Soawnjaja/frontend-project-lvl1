b79280ec546f764df0047eaf183f2d79303c8d96
Alex Wishinkiy <basstraper@gmail.com>
2023-08-25 21:02:26 +0300

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,9 @@
 import readlineSync from 'readline-sync';
 
-
-
-function greetingsUser() {
+const greetings = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  
-}
+};
 
-export { greetingsUser};
+export default greetings;
